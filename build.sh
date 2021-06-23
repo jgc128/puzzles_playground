be232bb7d626f96facacd9a9367d1bf562813bf6
Alexey Romanov <jgc128@outlook.com>
2021-06-23 12:09:53 -0400

    Build with a bash script

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-echo Test
+echo "Downloading Monaco Editor"
+curl https://registry.npmjs.org/monaco-editor/-/monaco-editor-0.25.2.tgz -o monaco.tgz
+mkdir monaco
+tar -xvzf monaco.tgz -C monaco/
+
+echo "Copying Monaco Editor to www"
+mkdir www/monaco
+cp -r monaco/package/min/vs www/monaco
+
+echo "Done!"
